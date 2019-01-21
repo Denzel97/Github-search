c0c52f2eb20e16a4eb5d6770bbcbb1c9060e09db
Denzel97 <denzelwash97@gmail.com>
2019-01-21 20:44:42 +0300

    add links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { ProfileService } from './services/profile.service';
 
@@ -14,7 +15,8 @@ import { ProfileComponent } from './components/profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
